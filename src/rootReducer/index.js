0a975801233d9c163e40285a9378fe9c2d7b86c1
paresh Mutha <pareshmutha1990@gmail.com>
2021-12-04 12:38:22 +0530

    Add files via upload

--- src/rootReducer/index.js
+++ src/rootReducer/index.js
@@ -1,11 +1,11 @@
 import {combineReducers} from 'redux';
 import appReducer from './appReducer';
-import allCustomers from '../pages/home/reducer';
 import loginData from '../pages/login/reducer';
+import traders from '../pages/traders/reducer';
 
 const rootReducer = combineReducers({
     appReducer,
-    allCustomers,
+    traders,
     loginData,
 });
 
